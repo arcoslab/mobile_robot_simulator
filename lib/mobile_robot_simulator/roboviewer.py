9c491caca76bd202df7511a5b7f0d0c12bc46da5
daniel garcia vaglio <degv364@gmail.com>
2018-05-25 08:31:23 -0600

    Add documentation to roboviewer

--- lib/mobile_robot_simulator/roboviewer.py
+++ lib/mobile_robot_simulator/roboviewer.py
@@ -26,6 +26,14 @@ cstyle.persistent = True
 class RoboviewerObjects():
     def __init__(self, portbasename, roboviewerportbasename, counter=-1,
                  wait_connection=False):
+        """
+        Initialize the communication interface with pyrovito
+
+        :param portbasename: base name of the local module
+        :param roboviewerportbasename: pyrovito basee name
+        :param counter: initial counter value
+        :param wait_connection: If roboviewer should wait for connections
+        """
         self.out_port = yarp.BufferedPortBottle()
         self.out_port.open(portbasename + "/roboviewer_out")
         yarp.Network.connect(portbasename + "/roboviewer_out",
@@ -38,6 +46,12 @@ class RoboviewerObjects():
         self.counter = counter
 
     def create_object(self, object_type):
+        """
+        Tell pyrovito to create an object
+
+        :param object_type: name of the type in avispy's object_lib
+        :return: The assigned object ID
+        """
         self.counter += 1
         bottle = self.out_port.prepare()
         bottle.clear()
@@ -49,6 +63,13 @@ class RoboviewerObjects():
         return (self.counter)
 
     def send_prop(self, object_id, prop_name, prop_data):
+        """
+        Tell pyrovito to update an object's state
+
+        :param object_id: number that identifies the object
+        :param prop_name: name of the parameter to change
+        :param prop_data: the parameter itself
+        """
         bottle = self.out_port.prepare()
         bottle.clear()
         bottle_list = bottle.addList()
@@ -59,6 +80,9 @@ class RoboviewerObjects():
         self.out_port.write(True)
 
     def send_prop_multi(self, object_ids, prop_names, prop_datas):
+        """
+        Same as send_prop but for multiple objects
+        """
         bottle = self.out_port.prepare()
         bottle.clear()
         for object_id, prop_name, prop_data in zip(object_ids, prop_names,
